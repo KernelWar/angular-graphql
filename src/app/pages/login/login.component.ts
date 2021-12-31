f653186ffad90d4443a32df3edcfbf3c9938f437
Sócrates López <josesocrateslopez@gmail.com>
2021-12-30 19:02:51 -0600

    interfaz login finalizado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   public hide = true;
+  public textHello = 'Good morning'
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    let hour = moment().hour()
+    if(hour < 12){
+      this.textHello = 'Good morning'
+    }else if(hour >= 12 && hour < 19){
+      this.textHello = 'Good afternon'
+    }else if(hour >= 19){
+      this.textHello = 'Good evening'
+    }
   }
 
 }
